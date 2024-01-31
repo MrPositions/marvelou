3b2aa8a87b1b3b8503ff71b82a25af5f181afd85
MrPositions <dotasmurf4444@gmail.com>
2024-02-01 04:51:31 +0500

    I'm so awesome

--- git this.py
+++ git this.py
@@ -1,2 +1,11 @@
 #linked to git
 #coming for those potatoes
+def create_points()
+    #create functions for bot
+    #create points to assign to players
+def create_points():
+    point = {'Value' : "value"}
+def create_character():
+    Character = {'C_name' : "c_name",'dare1' :"d1",'dare2' :"d2", 'dare3' :"d3"}
+
+    pass
